df16e027c27f8ad6cde112f2aeab21387309f740
Ayush MS <55694477+GrandpaHetRocks@users.noreply.github.com>
2021-08-19 02:36:36 +0530

    Add files via upload

--- Cluster vs Non Cluster/no_cluster_FL.py
+++ Cluster vs Non Cluster/no_cluster_FL.py
@@ -52,7 +52,7 @@ class Arguments():
     def __init__(self):
         self.images = 10000
         self.clients = 30
-        self.rounds = 4
+        self.rounds = 10
         self.epochs = 5
         self.local_batches = 64
         self.lr = 0.01

0f69fa8e37df8343eb5fd693c2c264f7a33bb03f
Ayush MS <ayushms0209@gmail.com>
2021-08-26 17:18:38 +0530

    Update test1.py

--- Cluster vs Non Cluster/test1.py
+++ Cluster vs Non Cluster/test1.py
@@ -95,8 +95,8 @@ class Net(nn.Module):
         super(Net, self).__init__()
         #self.quant = torch.quantization.QuantStub()
         self.conv1 = nn.Conv2d(1, 5, 5, 1)
-        self.conv2 = nn.Conv2d(5, 10, 5, 1)
-        self.fc1 = nn.Linear(4*4*10, 200)
+        self.conv2 = nn.Conv2d(5, 100, 5, 1)
+        self.fc1 = nn.Linear(4*4*100, 200)
         self.fc2 = nn.Linear(200, 10)
 
     def forward(self, x):
@@ -105,7 +105,7 @@ class Net(nn.Module):
         x = F.max_pool2d(x, 2, 2)
         x = F.relu(self.conv2(x))
         x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 4*4*10
+        x = x.view(-1, 4*4*100
                    )
         x = F.relu(self.fc1(x))
         x = self.fc2(x)

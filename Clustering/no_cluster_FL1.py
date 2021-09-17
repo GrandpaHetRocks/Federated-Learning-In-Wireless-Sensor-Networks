d5db7c5584fb649452f0ad2c1927cf0a11ce152d
Ayush MS <ayushms0209@gmail.com>
2021-09-18 02:01:46 +0530

    Update no_cluster_FL1.py

--- Clustering/no_cluster_FL1.py
+++ Clustering/no_cluster_FL1.py
@@ -53,8 +53,8 @@ class Arguments():
     def __init__(self):
         self.images = 10000
         self.clients = 30
-        self.rounds = 200
-        self.epochs = 2
+        self.rounds = 60
+        self.epochs = 3
         self.local_batches = 64
         self.lr = 0.01
         self.C = 0.9 #fraction of clients used in the round

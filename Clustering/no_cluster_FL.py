31f4a2918eb0807399639835ae6953c8262defbc
Ayush MS <ayushms0209@gmail.com>
2021-09-18 00:03:15 +0530

    --

--- Clustering/no_cluster_FL.py
+++ Clustering/no_cluster_FL.py
@@ -53,9 +53,9 @@ key_np=np.array(key1)
 class Arguments():
     def __init__(self):
         self.images = 10000
-        self.clients = 20
+        self.clients = 30
         self.rounds = 200
-        self.epochs = 2
+        self.epochs = 3
         self.local_batches = 64
         self.lr = 0.01
         self.C = 0.9 #fraction of clients used in the round

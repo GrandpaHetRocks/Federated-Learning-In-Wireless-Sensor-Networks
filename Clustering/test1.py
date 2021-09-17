95d1f7893e7d5d81234b2625c79b2237e4108dc1
Ayush MS <ayushms0209@gmail.com>
2021-09-18 02:54:21 +0530

    --

--- Clustering/test1.py
+++ Clustering/test1.py
@@ -47,7 +47,7 @@ class Arguments():
         self.images = 10000
         self.clients = 20
         self.rounds = 200
-        self.epochs = 2
+        self.epochs = 3
         self.local_batches = 64
         self.lr = 0.01
         self.C = 1 #fraction of clients used in the round

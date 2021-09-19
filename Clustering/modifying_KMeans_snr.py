31d1f51e26a7871aa890adb62f2981676d2b1055
Ayush MS <ayushms0209@gmail.com>
2021-09-20 02:04:50 +0530

    --

--- Clustering/modifying_KMeans_snr.py
+++ Clustering/modifying_KMeans_snr.py
@@ -139,7 +139,7 @@ def noise(clients):
             noise_list.append(['client'+str(i+1),'client'+str(j+1),noise])
     return(noise_list)
 
-def get_clusters(number=20):
+def get_clusters(number=30):
     cluster_array, _ = make_classification(n_samples=number, n_features=2, n_informative=2, n_redundant=0, n_clusters_per_class=1, random_state=50)#50
     #cluster_array, _ = make_blobs(n_samples=30,n_features=2, centers=2)
     #print(cluster_array)

a441adec5cda8416073cfb8b97481dc9aec706e1
Ayush MS <ayushms0209@gmail.com>
2021-09-20 19:01:59 +0530

    --

--- Clustering/no_cluster_FL.py
+++ Clustering/no_cluster_FL.py
@@ -104,8 +104,8 @@ class Net(nn.Module):
         #self.quant = torch.quantization.QuantStub()
         self.conv1 = nn.Conv2d(1, 5, 5, 1)
         self.conv2 = nn.Conv2d(5, 10, 5, 1)
-        self.fc1 = nn.Linear(4*4*10, 5)
-        self.fc2 = nn.Linear(5, 10)
+        self.fc1 = nn.Linear(4*4*10, 5) #20
+        self.fc2 = nn.Linear(5, 10) #20
 
     def forward(self, x):
         #x=self.quant(x)

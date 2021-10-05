8206fabbf3fab8676acaee4669fefb456ffd84aa
Ayush MS <ayushms0209@gmail.com>
2021-10-05 15:48:40 +0530

    Update modifying_KMeans_snr.py

--- Clustering/modifying_KMeans_snr.py
+++ Clustering/modifying_KMeans_snr.py
@@ -140,8 +140,9 @@ def noise(clients):
     return(noise_list)
 
 def get_clusters(number=30):
-    cluster_array, _ = make_classification(n_samples=number, n_features=2, n_informative=2, n_redundant=0, n_clusters_per_class=1, random_state=32)#50
+    #cluster_array, _ = make_classification(n_samples=number, n_features=2, n_informative=2, n_redundant=0, n_clusters_per_class=1, random_state=32)#50
     #cluster_array, _ = make_blobs(n_samples=30,n_features=2, centers=2)
+    cluster_array, _ = make_blobs(n_samples=number,n_features=2, centers=2,random_state=50)
     #print(cluster_array)
     no=1
     clients={}

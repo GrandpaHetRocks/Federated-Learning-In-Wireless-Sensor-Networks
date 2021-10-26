52e4b3aadbbabb6933ca8f7a871edb86f0475076
Ayush MS <ayushms0209@gmail.com>
2021-10-26 16:51:41 +0530

    Update FedAvg_Wireless_two_way_noise.py

--- Wireless X Federated/FedAvg_Wireless_two_way_noise.py
+++ Wireless X Federated/FedAvg_Wireless_two_way_noise.py
@@ -97,10 +97,10 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         #self.quant = torch.quantization.QuantStub()
-        self.conv1 = nn.Conv2d(1, 20, 5, 1)
-        self.conv2 = nn.Conv2d(20, 50, 5, 1)
-        self.fc1 = nn.Linear(4*4*50, 500)
-        self.fc2 = nn.Linear(500, 10)
+        self.conv1 = nn.Conv2d(1, 5, 5, 1)
+        self.conv2 = nn.Conv2d(5, 10, 5, 1)
+        self.fc1 = nn.Linear(4*4*10, 5) #..,50
+        self.fc2 = nn.Linear(5, 10) #50,5  50 non iid 5 iid
 
     def forward(self, x):
         #x=self.quant(x)
@@ -108,7 +108,7 @@ class Net(nn.Module):
         x = F.max_pool2d(x, 2, 2)
         x = F.relu(self.conv2(x))
         x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 4*4*50)
+        x = x.view(-1, 4*4*10)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
